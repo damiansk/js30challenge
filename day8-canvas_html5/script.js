3f686d0763254953a74be14564129e15a7c3fbcb
bumpMind <damian.s.ja@gmail.com>
2016-12-19 10:05:35 +0100

    Fist step - drawing line

--- day8-canvas_html5/script.js
+++ day8-canvas_html5/script.js
@@ -1,3 +1,44 @@
 /**
  * Created by bumpMind on 12/19/2016.
  */
+
+"use strict";
+
+( function () {
+
+    const canvas = document.querySelector( "#draw" );
+    const ctx = canvas.getContext( '2d' );
+
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+
+    ctx.strokeStyle = '#BADA55';
+    ctx.lineJoin = 'round';
+    ctx.lineCap = 'round';
+
+    let isDrawing = false;
+    let lastX = 0;
+    let lastY = 0;
+
+    function draw( e ) {
+        if ( !isDrawing ) return;
+
+        ctx.beginPath();
+        ctx.moveTo( lastX, lastY );
+        ctx.lineTo( e.offsetX, e.offsetY );
+        ctx.stroke();
+
+        [ lastX, lastY ] = [ e.offsetX, e.offsetY ];
+
+    }
+
+    canvas.addEventListener( 'mousemove', draw );
+    canvas.addEventListener( 'mousedown', ( e ) => {
+        isDrawing = true;
+        [ lastX, lastY ] = [ e.offsetX, e.offsetY ];
+    });
+    canvas.addEventListener( 'mouseup', () => isDrawing = false );
+    canvas.addEventListener( 'mouseout', () => isDrawing = false );
+
+
+})();
