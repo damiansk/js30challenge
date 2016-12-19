652bef3c4d30737596b56794b25a0176ddbd3295
bumpMind <damian.s.ja@gmail.com>
2016-12-19 09:43:24 +0100

    Day 7 done

--- day7-array_v2/script.js
+++ day7-array_v2/script.js
@@ -19,14 +19,54 @@
         { text: 'Ramen is my fav food ever', id: 123523 },
         { text: 'Nice Nice Nice!', id: 542328 }
     ];
+
     // Some and Every Checks
     // Array.prototype.some() // is at least one person 19?
+    const some = people.some( person => new Date().getFullYear() - person.year >= 19 );
+    // IN {} show whole object with attribute
+    console.log( { some } );
+
+
+
+
+
+
     // Array.prototype.every() // is everyone 19?
+    const every = people.every( person => new Date().getFullYear() - person.year >= 19);
+
+    console.log( every );
+
+
+
+
+
     // Array.prototype.find()
     // Find is like filter, but instead returns just the one you are looking for
     // find the comment with the ID of 823423
+    const find = comments.find( comment => comment.id === 823423 );
+
+    console.log( find );
+
+
+
+
+
     // Array.prototype.findIndex()
     // Find the comment with this ID
+    const findIndex = comments.findIndex( comment => comment.id === 823423 );
+
+    console.log( findIndex );
+
+
+
+
     // delete the comment with the ID of 823423
+
+    const newComments = [
+        ...comments.slice( 0, findIndex ),
+        ...comments.slice( findIndex + 1 )
+    ];
+
+    console.log( newComments );
 
 })();
