42251929489ba9fafc67d4969c6cad3f2005b563
bumpMind <damian.s.ja@gmail.com>
2016-12-20 14:24:25 +0100

    Day 10 done

--- day10-Shift&Checkboxes/script.js
+++ day10-Shift&Checkboxes/script.js
@@ -1,3 +1,38 @@
 /**
  * Created by bumpMind on 12/20/2016.
  */
+
+'use strict';
+
+( function () {
+
+    const checkboxes = document.querySelectorAll( '.inbox [type=checkbox]' );
+    let lastCheck;
+
+    function chandleCheck( e ) {
+
+        if ( e.shiftKey && this.checked ) {
+            let inBetween = false;
+
+            for ( let checkbox of checkboxes ) {
+
+                if ( checkbox === this || checkbox === lastCheck ) {
+                    inBetween = !inBetween;
+                }
+
+                if ( inBetween ) {
+                    checkbox.checked = true;
+                }
+            }
+        }
+
+        lastCheck = this;
+    }
+
+    for ( let checkbox of checkboxes ) {
+        checkbox.addEventListener( 'click', chandleCheck )
+    }
+
+
+
+} )();
