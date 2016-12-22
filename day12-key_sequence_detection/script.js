a55d1630fefaab4d05ffd4e8a67d7abaa49a6b88
bumpMind <damian.s.ja@gmail.com>
2016-12-22 09:01:42 +0100

    Day 12 done

--- day12-key_sequence_detection/script.js
+++ day12-key_sequence_detection/script.js
@@ -3,3 +3,22 @@
  */
 
 'use strict';
+
+( function () {
+
+    const pressed = [];
+    const secretCode = 'wesbos';
+
+    window.addEventListener( 'keyup', ( e ) => {
+        pressed.push( e.key );
+        pressed.splice( -secretCode.length - 1, pressed.length - secretCode.length );
+
+        if ( pressed.join( '' ).includes( secretCode ) ) {
+            cornify_add();
+        }
+
+        console.log( pressed );
+
+    } );
+
+} )();
