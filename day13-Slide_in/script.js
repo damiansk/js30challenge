4da327fe7d281ad693c2fdc0a9dbde57692d3d07
bumpMind <damian.s.ja@gmail.com>
2016-12-27 17:07:01 +0100

    Day 13 done

--- day13-Slide_in/script.js
+++ day13-Slide_in/script.js
@@ -1,3 +1,47 @@
 /**
  * Created by bumpMind on 12/23/2016.
  */
+
+'use strict';
+
+( function () {
+
+	function debounce( func, wait = 18, immediate = true ) {
+		let timeout;
+		return function() {
+			const context = this, args = arguments;
+
+			const later = function() {
+				timeout = null;
+				if ( !immediate ) {
+					func.apply( context, args );
+				}
+			};
+			const callNow = immediate && !timeout;
+			clearTimeout( timeout );
+			timeout = setTimeout( later, wait );
+			if ( callNow ) func.apply( context, args );
+		};
+	}
+
+	const sliderImages = document.querySelectorAll( '.slide-in' );
+
+	function checkSlide() {
+		for ( let sliderImage of sliderImages ) {
+			const slideInAt = ( window.scrollY + window.innerHeight ) - sliderImage.height / 2;
+			const imageBottom = sliderImage.offsetTop + sliderImage.height;
+
+			const isHalfShown = slideInAt > sliderImage.offsetTop;
+			const isNotScrolledPast = window.scrollY < imageBottom;
+
+			if ( isHalfShown && isNotScrolledPast ) {
+				sliderImage.classList.add( 'active' );
+			} else {
+				sliderImage.classList.remove( 'active' );
+			}
+		}
+	}
+
+	window.addEventListener( 'scroll', debounce( checkSlide ) );
+
+} )();
