72131f3c66dbbd5ba951185b32b09ecc20b6382d
bumpMind <damian.s.ja@gmail.com>
2017-01-02 10:21:58 +0100

    Day 18 done

--- day18-Tally_string_times/script.js
+++ day18-Tally_string_times/script.js
@@ -2,6 +2,25 @@
 
 ( function () {
 
+	const timeNodes = [...document.querySelectorAll( '[data-time]' )];
 
+	const seconds = timeNodes
+		.map( node => node.dataset.time )
+		.map( timeCode => {
+			const [ min, sec] = timeCode.split( ':' ).map( parseFloat );
+			return ( min * 60 ) + sec;
+		} )
+		.reduce( ( total, vidSeconds ) => total + vidSeconds);
+		// callback ( accumulator, currentValue ) - sth like foreach
+
+	let secondsLeft = seconds;
+
+	const hours = Math.floor( secondsLeft / 3600 );
+	secondsLeft %= 3600;
+
+	const minutes = Math.floor( secondsLeft / 60 );
+	secondsLeft %= 60;
+
+	console.log( hours, minutes, secondsLeft );
 
 } )();
