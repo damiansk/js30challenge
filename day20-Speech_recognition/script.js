4fc181020920d781504d53bd6006233449d10b35
bumpMind <damian.s.ja@gmail.com>
2017-01-05 14:08:56 +0100

    Day 20 done

--- day20-Speech_recognition/script.js
+++ day20-Speech_recognition/script.js
@@ -4,4 +4,33 @@
 
 	window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
 
+	const recognition = new SpeechRecognition();
+	recognition.interimResults = true;
+	recognition.lang = 'pl-PL';
+
+	let p = document.createElement( 'p' );
+	const classWords = document.querySelector( '.words' );
+
+	classWords.appendChild( p );
+
+	recognition.addEventListener( 'result', e => {
+		const transcript = [...e.results]
+			.map( result => result[0] )
+			.map( result => result.transcript )
+			.join( '' );
+
+
+		p.textContent = transcript;
+
+		if ( e.results[0].isFinal ) {
+			p = document.createElement( 'p' );
+			classWords.appendChild( p );
+		}
+
+	} );
+
+	recognition.addEventListener( 'end', recognition.start );
+
+	recognition.start();
+
 } )();
