a0f43e1b1d70660d5af9eac7c0d438b9b08ac339
bumpMind <damian.s.ja@gmail.com>
2017-01-05 12:27:12 +0100

    Day 19 done

--- day19-Webcam/script.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const video = document.querySelector('.player');
-const canvas = document.querySelector('.photo');
-const ctx = canvas.getContext('2d');
-const strip = document.querySelector('.strip');
-const snap = document.querySelector('.snap');
