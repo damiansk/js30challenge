a00845b341ae76795abae3a63807de46e12a1d44
bumpMind <damian.s.ja@gmail.com>
2017-02-03 15:19:03 +0100

    Day 30 done

--- day30-Whack_a_mole_game/script.js
+++ day30-Whack_a_mole_game/script.js
@@ -1,12 +1,68 @@
 'use strict';
 
+
 ( function () {
 
 	const holes = document.querySelectorAll( '.hole' );
 	const scoreBoard = document.querySelector( '.score' );
 	const moles = document.querySelectorAll( '.mole' );
 
+	let lastHole;
+	let score;
+	let timeUp = false;
 
+	function randomTime ( min, max) {
+		return Math.round( Math.random() * ( max - min ) + min );
+	}
+
+	function randomHole ( holes ) {
+		const index = Math.floor( Math.random() * ( holes.length ) );
+		const hole = holes[index];
+
+		if ( lastHole === hole ) {
+			return randomHole( holes );
+		}
+
+		lastHole = hole;
+		return hole;
+	}
+
+	function peep () {
+		const time = randomTime( 200, 1000 );
+		const hole = randomHole( holes );
+
+		hole.classList.add( 'up' );
+
+		setTimeout( () => {
+			hole.classList.remove( 'up' );
+			if ( !timeUp ) {
+				peep();
+			}
+		}, time );
+	}
+
+	function startGame () {
+		scoreBoard.textContent = 0;
+		score = 0;
+		timeUp = false;
+
+		peep();
+
+		setTimeout( () => timeUp = true, 10000 );
+	}
+
+	function bonk ( e ) {
+		if ( !e.isTrusted ) return;
+
+		score++;
+		this.classList.remove( 'up' );
+		scoreBoard.textContent = score;
+	}
+
+
+	for ( let mole of moles ) {
+		mole.addEventListener( 'click', bonk );
+	}
 
 	document.querySelector( '#start-button' ).addEventListener( 'click', startGame );
 
