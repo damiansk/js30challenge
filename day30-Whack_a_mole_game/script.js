4389a1da17e6f1b9a0042206c340d5980dd2451a
bumpMind <damian.s.ja@gmail.com>
2017-02-03 14:35:17 +0100

    Added files for day 30

---
+++ day30-Whack_a_mole_game/script.js
@@ -0,0 +1,13 @@
+'use strict';
+
+( function () {
+
+	const holes = document.querySelectorAll( '.hole' );
+	const scoreBoard = document.querySelector( '.score' );
+	const moles = document.querySelectorAll( '.mole' );
+
+
+
+	document.querySelector( '#start-button' ).addEventListener( 'click', startGame );
+
+} )();
